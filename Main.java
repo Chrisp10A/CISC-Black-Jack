5e0f2fdd05a6e4bbe089edafefc4f15100ee36b3
Zoomba145 <chrisstaley593@gmail.com>
2025-11-18 05:06:40 +0000

    Added a simple java script to our project.

--- Main.java
+++ Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Welcome! This is CISC-Black-Jack");
+		Scanner input = new Scanner(System.in);
+
+        // Ask for the user's name
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        System.out.print("");
+		System.out.print("Hello, " + name + " Are you up for a game of Blackjack?");
+		String answer = input.nextLine();
+		if (answer.equals("Yes")) {
+			System.out.println("Yay!");
+		}
+		else {
+			System.out.println("Aw man :(");
+		}
+		// stops
+		input.close();
 	}
 }
