79241fbfe007ba2c2d89fda36d1b92eeab8cc1c2
Zoomba145 <chrisstaley593@gmail.com>
2025-11-19 02:17:00 +0000

    Added variables

--- Main.java
+++ Main.java
@@ -2,7 +2,24 @@
 import java.util.Scanner;
 public class Main
 {
+	// Declare Variables
+	// Final
+	final String finalSuits = "♤♡♧♢";
+	final String finalFaceCardNames = "JQKA";
+	// Reset
+	int totalAces;
+	int totalPlayer;
+	int totalDealer;
+	int totalDealerAces;
+	int totalDealerHasInsurance;
+	int totalDealerCards;
+	boolean hasInsurance;
+	boolean DealerHasAce;
+	String activeDealerCards;
+	
+
 	public static void main(String[] args) {
+		int chips = 100;
 		System.out.println("Welcome! This is CISC-Black-Jack");
 		Scanner input = new Scanner(System.in);
 
@@ -10,15 +27,34 @@ public class Main
         System.out.print("Enter your name: ");
         String name = input.nextLine();
         System.out.print("");
-		System.out.print("Hello, " + name + " Are you up for a game of Blackjack?");
+		System.out.println("Hello, " + name + ". Are you up for a game of Blackjack?");
+		System.out.print("Type 'Y' for yes and 'N' for No");
 		String answer = input.nextLine();
-		if (answer.equals("Yes")) {
+		// Should probably use case stuff
+		if (answer.equals("Y")) {
 			System.out.println("Yay!");
 		}
 		else {
 			System.out.println("Aw man :(");
 		}
 		// stops
+		System.out.println("Starting Game...");
+		// GAME LOOP
+		Main MainInstance = new Main();
+		MainInstance.reset();
+		
 		input.close();
+	}
+
+	public void reset()
+	{
+		System.out.print("reset");
+		totalAces = 0;
+		totalPlayer = 0;
+		totalDealerAces = 0;
+		hasInsurance = false;
+		DealerHasAce = false;
+		totalDealerCards = 0;
+		activeDealerCards = null;
 	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Hey guys
+# Hey guys soooo lets go make this thing!
+# I am using:
+# https://scratch.mit.edu/projects/1203717623/ 
+# as a model for the code.
+# Also look at the document pls.
