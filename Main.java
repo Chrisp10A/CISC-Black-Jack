c6b2495ad09db6fbe8faa3f49894a20f3dc14c86
Zoomba145 <chrisstaley593@gmail.com>
2025-12-02 04:44:09 +0000

    Added more comments

--- Main.java
+++ Main.java
@@ -46,13 +46,13 @@ public class Main
 			Main MainInstance = new Main();
 			// Bypass static >:D
 			MainInstance.introForGame(input);	
-      		// stops
+      		// After intro
 			System.out.println("Starting Game");
 			// GAME LOOP
 			while (true) {
 			System.out.println("----------------------");
 			MainInstance.reset();
-			// Maybe something with case
+			// Maybe something with case + start game
 			MainInstance.newRound(input);
 			if (MainInstance.checkLose()) {
 				System.out.println("Game Over! You ran out of chips");
@@ -68,6 +68,7 @@ public class Main
 	}
 
 	public void introForGame(Scanner input) {
+		// Ask for name and asks for game
 		System.out.print("Enter your name: ");
 		name = input.nextLine();
 		System.out.println("");
@@ -110,6 +111,7 @@ public class Main
 
 	public void reset()
 	{
+		// Reset all variables
 		totalAces = 0;
 		totalPlayer = 0;
 		totalDealer = 0;
@@ -125,6 +127,7 @@ public class Main
 	}
 	
 	public void createDeck(){
+		// Create two deck, one for the suits (char) and numbers (int)
 		suitDeck = new ArrayList<>();
 		numberDeck = new ArrayList<>();
 		for (int createSuit = 0; createSuit < 4; createSuit++) {
@@ -139,6 +142,8 @@ public class Main
 	}
 
 	public void shuffleDeck(){
+		// Chooses a number and matches it to both decks, put both both into their shuffled deck variants
+		// 2-10 are number cards while 11-14 are face cards with aces being 14
 		shuffledSuitDeck = new ArrayList<>();
 		shuffledNumberDeck = new ArrayList<>();
 		Random random = new Random();
@@ -148,6 +153,7 @@ public class Main
 			} catch (Exception e) {
 				randomCard = 0;
 			}
+			// Adds the chosen card to both decks, then removes to avoid duplicates
 			shuffledNumberDeck.add(numberDeck.get(randomCard));
 			shuffledSuitDeck.add(suitDeck.get(randomCard));
 			numberDeck.remove(randomCard);
@@ -157,6 +163,7 @@ public class Main
 
 	public void newRound(Scanner input)
 	{
+		// Prompt user for chips
 			System.out.println("How many chips are you betting? You have: " + totalChips + " chips");
 			do { 
 				try {
@@ -168,11 +175,12 @@ public class Main
 			} while ((0 >= inputInt) || (inputInt > totalChips));
 		System.out.println("");
 		intialDraw();
-		// Insurance be like
+		// If the Player has enough chips and Dealer has an ace (insurance)
 		if ((DealerHasAce) && ((int) (Math.floor(betChips / 2)) + betChips) <= totalChips) {
 			betInsuranceChips = (int) (Math.floor(betChips / 2));
 			dealerHasAce(input);
 		}
+		// If either Dealer or Player draws Blackjack (Ace + face card or 10)
 		if ((totalDealer == 21) || (totalPlayer == 21)){
 			HasBlackjack();
 		}
@@ -182,6 +190,7 @@ public class Main
 	}
 	
 	public void intialDraw(){
+		// Draws two cards for the Dealer and Player
 		betChips = inputInt;
 		if (totalChips == betChips) {
 			System.out.print("YOU GO ALL IN! ");
@@ -189,50 +198,55 @@ public class Main
 		System.out.println("You bet " + inputInt);
 		System.out.print("The Dealer draws: ");
 		/*
-		shuffledNumberDeck.set(0, 14);
-		shuffledNumberDeck.set(1, 10);
+		Used for testcases:
+		shuffledNumberDeck.set(0, 5);
+		shuffledNumberDeck.set(1, 14);
 		*/
 		dealerDrawCard();
 		dealerDrawCard();
 		System.out.println("");
 		System.out.print(name + " draws: ");
-		/*
-		shuffledNumberDeck.set(0, 14);
-		shuffledNumberDeck.set(1, 10);
-		*/
 		playerDrawCard();
 		playerDrawCard();
 		System.out.println("Total: " + totalPlayer);
 	}
 
 	public void dealerDrawCard(){
-		// Yo, pls make this an abstract method maybe?
+		// Abstract method with playerDrawCard?
+		// "Draws the card and incrments how many cards are in the Dealer's hand"
 		activeCardNumber = shuffledNumberDeck.get(0);
 		totalDealerCards = totalDealerCards + 1;
+		// If it's a number card add value to the total value of the dealer
 		if (activeCardNumber < 11){
 			totalDealer = (totalDealer + activeCardNumber);
 		}
 		else {
+			// If it's an ace and it's the dealer's second card
 			if (activeCardNumber == 14) {
 				if (totalDealerCards == 2){
 					DealerHasAce = true;
 				}
+				// Adds total value of ace to total value and if it goes over decrease value by 10
 				totalDealer = (totalDealer + 11);
 				if (21 < totalDealer) {
 					totalDealer = (totalDealer - 10);
 				}
+				// If ace didn't have to decrease value, log it for next time.
 				else {
 					totalDealerAces = (totalDealerAces + 1);
 				}
 			}
+			// All face cards are worth 10
 			else {
 				totalDealer = (totalDealer + 10);
 			}
 		}
+		// If dealer is over 21 but has an ace that can decrease in value
 		if ((21 < totalDealer) && (totalDealerAces > 0)){
 			totalDealer = (totalDealer - 10);
 			totalDealerAces = (totalDealerAces - 1);
 		}
+		// Hides the first card value from the player
 		if (totalDealerCards == 1){
 			System.out.print("XX ");
 		}
@@ -242,6 +256,7 @@ public class Main
 	}
 
 	public void playerDrawCard(){
+		// Same is dealerDrawCard but no cards are hidden
 		activeCardNumber = shuffledNumberDeck.get(0);
 		if (activeCardNumber < 11){
 			totalPlayer = (totalPlayer + activeCardNumber);
@@ -270,6 +285,7 @@ public class Main
 	}
 
 	public void convertCardToString(){
+		// Convert int to a String to be printed out
 		if (activeCardNumber < 11) {
 			activeCard = ("" + activeCardNumber);
 		}
@@ -279,25 +295,31 @@ public class Main
 	}
 
 	public void printDrawCards(boolean isDealer){
-		activeCard = ("" + activeCard + shuffledSuitDeck.get(0) + " ");
+		// Prints the activeCard with the (Number) + (Suit) and a space at the end
+		activeCard = (activeCard + shuffledSuitDeck.get(0) + " ");
+		// Does not print if this is the Dealer's first card
 		if ((!isDealer) || (1 != totalDealerCards)) {
 			System.out.print(activeCard + "");
 		}
+		// Logs cards for later if Dealer
 		if (isDealer){
 			activeDealerCards = (activeDealerCards + activeCard);
 		}
 	}
 
 	public void deleteCard(){
+		// Remove card from both lists
 		shuffledNumberDeck.remove(0);
 		shuffledSuitDeck.remove(0);
 	}
 
 	public void HasBlackjack(){
+		// Check if Player has Blackjack
 		if (totalPlayer == 21) {
 			System.out.println(name + " has Blackjack!");
 		}
 		dealerPlay();
+		// Check if Dealer had Blackjack
 		if (totalDealer == 21) {
 			if (totalPlayer == 21) {
 				System.out.println("The Dealer also had Blackjack!");
@@ -305,7 +327,8 @@ public class Main
 			else {
 				System.out.println("The Dealer has Blackjack!");
 			}	
-		}		
+		}	
+		// Check if both Dealer and Player have Blackjack	
 		if ((totalDealer == 21) && (totalPlayer == 21)) {
 			System.out.println("Stand-off. Both Player and Dealer hava a total of 21.");
 			if (hasInsurance) {
@@ -316,6 +339,7 @@ public class Main
 			System.out.println("No change");
 			}
 		}
+		// Checks if Dealer has Blackjack for outcome
 		else {
 			if (totalDealer == 21) {
 				totalChips = totalChips - betChips;
@@ -335,8 +359,10 @@ public class Main
 
 	public void startRound(Scanner input)
 	{
+		// Local Variable
 		boolean isValid;
 		do { 
+				// If you can Double Down (Needs to be first time asking and have enough to bet)
 				if ((betChips * 2 <= totalChips) && (firstAction)) {
 					System.out.println("Do you Hit (H), Stand (S), or Double Down (D)?");
 				}  
@@ -349,6 +375,7 @@ public class Main
 					} catch (Exception e) {
 						System.out.println("ERROR: Please type a valid letter");
 					}
+					// If it was a valid action
 					if ((inputChar == 'H') || (inputChar == 'S') || ((inputChar == 'D') && (betChips * 2 <= totalChips) && (firstAction))) {
 						firstAction = false;
 						isValid = true;
@@ -358,30 +385,37 @@ public class Main
 						isValid = false;
 					}
 				} while (!isValid);
+				// Adds a space for readablity
 				System.out.println("");
 				checkHit();
+				// If action ends round
 			} while (!((inputChar == 'S') || (inputChar == 'D') || (21 < totalPlayer)));
 		endRound();
 	}
 
 	public void checkHit(){
+		// If Double Down
 		if (inputChar == 'D') {
 			betChips = betChips * 2;
 			System.out.println("Double Down! Bet raised to " + (betChips));
 			hit();
 		}
+		// If Hit
 		if (inputChar == 'H') {
 			hit();
 		}
 	}
 
 	public void endRound(){
-		// Could probably use a case statement here, maybe ._.
+		// Could probably use a case statement here
+		// If player busted
 		if (totalPlayer > 21){
 			loseBusted();
 		}
 		else {
+			// Prints total value of player
 			System.out.print("Stand Total: " + (totalPlayer));
+			// Adds a '!' if exactly 21
 			if (totalPlayer == 21) {
 				System.out.println("!");
 			}
@@ -389,14 +423,17 @@ public class Main
 				System.out.println("");
 			}
 			dealerPlay();
+			// If Dealer busted
 			if (21 < totalDealer) {
 				winRound();
 			}
 			else {
+				// If Dealer and Player tied
 				if (totalDealer == totalPlayer) {
 					tieRound();
 				}
 				else {
+					// Check if Player is higher than Dealer, if yes then win
 					if (totalDealer < totalPlayer) {
 						winRound();
 					}
@@ -409,12 +446,14 @@ public class Main
 	}
 
 	public void hit(){
+		// Prints Hit! and the new total of the Player
 		System.out.print("Hit! ");
 		playerDrawCard();
 		System.out.println("Total " + totalPlayer);
 	}
 
 	public void dealerPlay(){
+		// Dealer hits until obove 17 in total value
 		System.out.println("The Dealer has " + activeDealerCards + " Total: " + totalDealer);
 		while (totalDealer < 17) {
 			System.out.print("The Dealer draws: ");
@@ -424,12 +463,14 @@ public class Main
 	}
 
 	public void loseBusted(){
+		// Prints lose text on screen and decreases chips by amount bet
 		System.out.println("Busted. " + totalPlayer + " is over 21");
 		totalChips = totalChips - betChips;
 		System.out.println("You lost " + betChips + " chips");
 	}
 
 	public void winRound(){
+		// Prints win text and increases chips by amount bet, also accounts for if the Dealer busted instead comparing
 		totalChips = totalChips + betChips;
 		if (21 < totalDealer) {
 			System.out.println("Busted " + totalDealer + " is over 21.");
@@ -442,26 +483,32 @@ public class Main
 	}
 
 	public void tieRound(){
-		System.out.println("Tie! Both dealer and player had " + totalPlayer);
+		// Prints text if tied
+		System.out.println("Stand-off. Both dealer and player had " + totalPlayer);
 		System.out.println("No change");
 	}
 
 	public void loseRound(){
+		// Prints lose text on screen, decreases chips by amount bet, and prints both values
 		System.out.println("Loss. You had " + totalPlayer + " while the dealer had " + totalDealer);
 		totalChips = totalChips - betChips;
 		System.out.println("You lost " + betChips + " chips");
 	}
 
 	public void dealerHasAce(Scanner input){
+		// Prompts user for if they want insurance
 		System.out.println("The Dealer has an Ace, do you wish to buy insurance? (Y/N) (" + betInsuranceChips + " chips)");
 		do { 
 				inputChar = input.next().charAt(0);
 			} while (!((inputChar == 'Y') || (inputChar == 'N')));
 		if (inputChar == 'Y') {
+			// If the dealer has 21 sets the boolean true for bought insurance for later
 			if (totalDealer == 21) {
 				hasInsurance = (true);
 			}
+			// If bought insurance but Dealer did not have Blackjack
 			else {
+				// Lose amount bet for insurance and prints no insurance text
 				totalChips = totalChips - betInsuranceChips;
 				System.out.println("Dealer does NOT have Blackjack");
 				System.out.println("You lost " + betInsuranceChips + " chips");
