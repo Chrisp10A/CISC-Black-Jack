fef92dc0e5e0da10cd12b628be26d0e2a6649cde
Hiro0551 <piercedavy3@gmail.com>
2025-12-14 05:55:50 +0000

    Clock2 - Expanded and Enhanced

--- Clock2.java
+++ Clock2.java
@@ -1,6 +1,13 @@
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 public class Clock2 {
-    public static void printtime(){{
-        System.out.println(LocalTime.now());
-    }}
+
+    public static void printtime() {
+        LocalTime currentTime = LocalTime.now();
+        String pattern = "hh:mm:ss a";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        String formattedTime = currentTime.format(formatter);
+        System.out.println("Current Time: " + formattedTime);
+    }
 }
