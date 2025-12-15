6c1a524ec7e97453f157abd2cc04ea2cf58ff8f5
Chris Staley <chrisstaley593@gmail.com>
2025-12-14 22:11:06 -0800

    Added Launcher

---
+++ src/main/java/com/example/javafxblackjack/Launcher.java
@@ -0,0 +1,11 @@
+package com.example.javafxblackjack;
+
+import javafx.application.Application;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        Application.launch(BlackJack.class, args); 
+    }
+
+}
